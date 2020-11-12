e83ea6e106f01e3125c7b5d5413b616d72df6d54
Adnan Zaman <adnanzmn@hotmail.com>
2020-11-12 18:02:05 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const indexController = require("../controllers/indexController");
+const router = express.Router();
+
+router.get("/", indexController.index_get);
+router.post("/", indexController.index_post);
+
+module.exports = router;

b975d90d20e96ee21496553fe023dbc3ee55f7ab
Adnan Zaman <adnanzmn@hotmail.com>
2021-01-13 18:40:56 -0500

    change fave game positions using drag and drop

--- components/Game.js
+++ components/Game.js
@@ -1,10 +1,56 @@
+import { useState, useRef, useEffect } from "react";
+
 export default function Game(props) {
-  console.log(`i am ${props.gameName} and editing is ${props.isEditing}`);
+  const [clicked, setClicked] = useState(false);
+  const [y, setY] = useState(0);
+
+  //   console.log(`i am ${props.gameName} and I am clicked: ${clicked}`);
+
+  //   function moveFunc(e) {
+  //     e.preventDefault();
+  //     if (clicked) setY(y + e.movementY);
+  //   }
+
+  function stopDrag(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  const undraggable = {
+    draggable: "false",
+    onDragStart: stopDrag,
+    onDragOver: stopDrag,
+    onDrop: stopDrag,
+  };
+
   return (
-    <div className="game-container">
-      <img src={props.gameCoverUrl} alt={`Cover art for ${props.gameName}`} />
-      <p>{props.gameName}</p>
-      {props.isEditing && <button>Delete</button>}
+    <div
+      className="game-container"
+      data-index={props["data-index"]}
+      onDragStart={props.onDragStart}
+      onDragOver={props.onDragOver}
+      onDrop={props.onDrop}
+      draggable={props.draggable}
+    >
+      <img
+        src={props.gameCoverUrl}
+        alt={`Cover art for ${props.gameName}`}
+        {...undraggable}
+      />
+      <p {...undraggable}>{props.gameName}</p>
+      {props.isEditing && <button {...undraggable}>Delete</button>}
     </div>
   );
 }
+
+// onMouseDown={(e) => {
+//     e.preventDefault();
+//     setClicked(true);
+//   }}
+//   onMouseUp={() => setClicked(false)}
+//   onMouseMove={moveFunc}
+// style={{
+//     position: "relative",
+//     top: `${y}px`,
+//     borderColor: clicked ? "red" : "black",
+//   }}
--- pages/userPage.js
+++ pages/userPage.js
@@ -7,20 +7,54 @@ export default function UserPage(props) {
   const [currFavoriteGames, setCurrFavoriteGames] = useState(favoriteGames);
   const [isEditing, setIsEditing] = useState(false);
 
-  const favoriteGamesList = favoriteGames.map((game) => (
+  function dragFunc(e) {
+    //e.preventDefault();
+    e.stopPropagation();
+    console.log("this is drag func");
+    e.dataTransfer.setData("text/plain", e.target.dataset.index);
+  }
+
+  function dragOver(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("this is dragover");
+    e.dataTransfer.dropEffect = "move";
+  }
+
+  function drop(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("this is drop");
+    const newFaveGames = [...favoriteGames];
+    const i = Number(e.target.dataset.index);
+    const j = Number(e.dataTransfer.getData("text/plain"));
+    //console.log(e.target);
+    console.log(`${i} ${j}`);
+    const temp = newFaveGames[i];
+    newFaveGames[i] = newFaveGames[j];
+    newFaveGames[j] = temp;
+    setFavoriteGames(newFaveGames);
+  }
+
+  const favoriteGamesList = favoriteGames.map((game, index) => (
     <Game
       key={game.id}
       gameId={game.id}
       gameName={game.name}
       gameCoverUrl={game.coverurl}
       isEditing={isEditing}
+      data-index={"" + index}
+      draggable="true"
+      onDragStart={dragFunc}
+      onDragOver={dragOver}
+      onDrop={drop}
     />
   ));
 
   return (
     <>
       <h1>{props.displayName}</h1>
-      <div>{favoriteGamesList}</div>
+      <div className="favorite-games-list">{favoriteGamesList}</div>
       {props.id && <button onClick={() => setIsEditing(true)}>Edit</button>}
     </>
   );

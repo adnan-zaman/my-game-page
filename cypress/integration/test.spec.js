e9d40c4470b80a88e885fa4d80df1e504a0c2ac6
Adnan Zaman <adnanzmn@hotmail.com>
2021-02-17 13:49:20 -0500

    more test

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,7 +1,5 @@
 describe("my first test", function () {
   it("should return true", function () {
-    cy.visit("/");
-    cy.get("#form-field-Email-login-form").type("test@abc.com");
-    cy.screenshot();
+    expect(30).to.eq(30);
   });
 });

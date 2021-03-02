f7ee3c00dcfc43dc0a585d59521da1132ca1e7e5
Adnan Zaman <adnanzmn@hotmail.com>
2021-03-02 10:12:55 -0500

    Delete cypress directory

--- cypress/integration/test.spec.js
+++
@@ -1,7 +0,0 @@
-describe("my first test", function () {
-  it("should return true", function () {
-    cy.visit("/");
-    cy.get("#form-field-Email-login-form").type("test@abc.com");
-    cy.screenshot();
-  });
-});
